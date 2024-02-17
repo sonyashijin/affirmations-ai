265aefaed845d87e1ccc71cdc65608e0912d6e8f
Joshua Delgadillo <joshdelg@stanford.edu>
2024-02-17 14:55:11 -0800

    Added react native repo

---
+++ affirmations-ai/App.js
@@ -0,0 +1,16 @@
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import "react-native-get-random-values";
+import { CONVEX_URL } from "@env";
+import Tasks from "./Tasks";
+
+const convex = new ConvexReactClient(CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
+
+export default function App() {
+  return (
+    <ConvexProvider client={convex}>
+      <Tasks />
+    </ConvexProvider>
+  );
+}
