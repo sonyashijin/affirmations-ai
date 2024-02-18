6fa06f60e413c150c4dccef26bba83db13c6ffc6
sonyashijin <65740491+sonyashijin@users.noreply.github.com>
2024-02-17 23:42:37 -0800

    added navigation to home

--- affirmations_app/Onboarding.js
+++ affirmations_app/Onboarding.js
@@ -52,7 +52,7 @@ class Onboarding extends React.Component {
                 <Button
                   style={styles.button}
                   color={argonTheme.COLORS.SECONDARY}
-                  onPress={() => navigation.navigate("App")}
+                  onPress={() => navigation.navigate("Start")}
                   textStyle={{ color: argonTheme.COLORS.BLACK }}
                 >
                   Get Started
